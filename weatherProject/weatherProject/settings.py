fbcd37edb8583f34315c1e6c87be05a96e4a8296
kamala-mohan <69154342+kamala-mohan@users.noreply.github.com>
2023-01-29 09:44:36 +0530

    Update settings.py

--- weatherProject/weatherProject/settings.py
+++ weatherProject/weatherProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-7)y)4b61bszx6m5dz2g7k7+^i1@-#fel^#8zi&sz@-@uxshjn%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
